03f85e7ef4f4511c400feda5d12b6fcd323969d0
aml <17839166546@163.com>
2019-10-24 15:45:17 +0800

    1. 考试名称的规范与拆解成数据库中的名称

--- src/main/java/com/zgczx/utils/DateFormatUtil.java
+++ src/main/java/com/zgczx/utils/DateFormatUtil.java
@@ -12,6 +12,7 @@ import java.util.Date;
 /**
  * 日期格式化工具
  * 将考试名称中的日期格式化为: xxxx年xx月
+ *
  * @author aml
  * @date 2019/10/24 10:56
  */
@@ -19,60 +20,75 @@ import java.util.Date;
 public class DateFormatUtil {
 
     @Autowired
-    static ExamInfoDao examInfoDao;
+    ExamInfoDao examInfoDao;
 
-    public static String dateFormat(String dateString) throws Exception{
-
+    /**
+     * 将库中不规范的考试名称封装为正确规范格式
+     * @param dateString 数据库中的考试名称
+     * @return
+     * @throws Exception
+     */
+    public String dateFormat(String dateString) throws Exception {
+        log.info("原始dataString字符串: {}", dateString);
         int year = dateString.indexOf("年");
-        if (year == 2){
-            dateString = "20"+dateString;
-        }else if (year == 3){
-            dateString = "2" +dateString;
+        if (year == 2) {
+            dateString = "20" + dateString;
+        } else if (year == 3) {
+            dateString = "2" + dateString;
         }
         int c = dateString.indexOf("月");
-        String substring = dateString.substring(c+1,dateString.length());
+        String substring = dateString.substring(c + 1, dateString.length());
         dateString = dateString.substring(0, c + 1);
-        System.out.println("输出1："+substring);
-        System.out.println("输出2："+dateString);
+
+        log.info("截取的前部分字符中： {}", dateString);
+        log.info("截取的后部分字符中： {}", substring);
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月");
         String format = null;
         if (dateString.indexOf("年") != -1) {
             Date parse = null;
-
             parse = dateFormat.parse(dateString);
             format = dateFormat.format(parse);
         }
+        String standardFormat = format+substring;
+        log.info("正确格式的字符串: {}",standardFormat );
 
-            System.out.println("s:" + format);
-
-        System.out.println("完整:" + format+substring);
-        return format+substring;
+        return standardFormat;
     }
 
 
-    public static String recoveryString(String dateString){
-       //1. 先用传来的参数，查库
+    /**
+     * 将前端传来的正确规范的考试名称，拆分为数据库中的考试名称，用于真正调用接口
+     * @param dateString 规范的考试名称格式
+     * @return
+     */
+    public String recoveryString(String dateString) {
+        //1. 先用传来的参数，查库
         ExamInfo examName = examInfoDao.getByExamName(dateString);
         System.out.println(examName);
-        //2. 查出
-        if (examName.equals(dateString)){
-            return dateString;
-        }else {
-            //3. 查不出
-            dateString = dateString.substring(2,dateString.length());
 
-
-            return null;
+        if (examName != null) {
+            //2. 查出
+            if (examName.getExamName().equals(dateString)) {
+                System.out.println("查出来了： " + dateString);
+                return dateString;
+            }
         }
+        //3. 查不出
+        dateString = dateString.substring(2, dateString.length());
+        //3.2 用截取过后的字符串，再查库
+        ExamInfo examName1 = examInfoDao.getByExamName(dateString);
+        if (examName1 != null) {
+            if (dateString.equals(examName1.getExamName())) {
+                System.out.println(dateString);
+                return dateString;
+            }
+        }
+        //替换字符串中的第一个0,
+        dateString = dateString.replaceFirst("0", "");
+        return dateString;
     }
 
 
-
-    public static void main(String[] args)throws Exception{
-       String s = "2019年4月月考期末";
-        recoveryString("19年4月期中");
-//       dateFormat(s);
-    }
 
 }
